f8a5f38e67ac3809c88b196dee76d73cd8b2e62d
Uziel Gaeta <ugaeta@cpp.edu>
2021-10-26 00:39:15 -0700

    Finished java snake game

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,6 +3,16 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame{
     
     GameFrame(){
+        
+        GamePanel panell = new GamePanel();
+        
+        this.add(panell);
+        this.setTitle("Java Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
 
     }
 }
