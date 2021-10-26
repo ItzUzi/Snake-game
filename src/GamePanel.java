0bd07e3c740192891b08abb03f2c5c293907eb72
Uziel Gaeta <ugaeta@cpp.edu>
2021-10-25 17:16:01 -0700

    Started on java snake game

---
+++ src/GamePanel.java
@@ -0,0 +1,17 @@
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+    GamePanel(){
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
